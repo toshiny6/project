632a45fe700c992a52848a6d187b31175b6269ed
JongSeongYang <60866755+JongSeongYang@users.noreply.github.com>
2021-04-04 16:57:53 +0900

    title bar 제거 / 로딩 화면 추가

---
+++ app/src/main/java/com/example/camera/SplashActivity.kt
@@ -0,0 +1,26 @@
+package com.example.camera
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+
+class SplashActivity : AppCompatActivity() {
+
+    private val time: Long = 1000
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+
+        CoroutineScope(Dispatchers.IO).launch {
+            delay(time)
+            val intent = Intent(applicationContext, MainActivity::class.java)
+            startActivity(intent)
+            finish()
+        }
+    }
+}
